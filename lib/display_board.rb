6c892a14074f4a671329a656ad6d6a2fefa2bf92
Grace Carter <graciec@gmail.com>
2017-12-20 16:01:41 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -15,7 +15,7 @@ def display_board(board)
   puts "   |   |   "
 end
 def display_board(board1)
-  puts "   |   |  #{board[2]}    "
+  puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
